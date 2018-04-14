ad2de83ceaf6293d18e78412be5d63b7251883ff
Sam <mcmyler@gmail.com>
2018-04-14 13:58:29 -0500

    Persist state to localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,15 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 
-// import { routerReducer, routerMiddleware } from "react-router-redux";
-
 import createHistory from "history/createBrowserHistory";
 
 import thunk from "redux-thunk";
+import persistState from "redux-localstorage";
 import loggingMiddleware from "./middleware/loggingMiddleware";
 
 import reducers from "./reducers"; // Or wherever you keep your reducers
 
 // Create a history of your choosing (we're using a browser history in this case)
 export const history = createHistory();
-
-// Build the middleware for intercepting and dispatching navigation actions
-// const middleware = routerMiddleware(history);
 
 // Add the reducer to your store on the `router` key
 // Also apply our middleware for navigating
@@ -22,7 +18,6 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(
   combineReducers({
     ...reducers
-    // router: routerReducer
   }),
-  composeEnhancers(applyMiddleware(/*middleware,*/ thunk, loggingMiddleware))
+  composeEnhancers(applyMiddleware(thunk, loggingMiddleware), persistState())
 );
